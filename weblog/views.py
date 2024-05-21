30dc6dab59c50131afaa76f245ce1fe2e563e233
amirhosein <amirhoseinbu80@gmail.com>
2024-05-21 19:25:50 +0330

    build home view

--- weblog/views.py
+++ weblog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.decorators.http import require_http_methods
 # Create your views here.
+
+require_http_methods(["GET"])
+def index(request):
+
+    context={}
+
+    return render(request,'blog/home.html',context)
