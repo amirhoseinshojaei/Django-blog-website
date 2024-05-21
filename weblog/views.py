8cfab940dcb9a7335fe9508b2ae2db5d81510cbb
amirhosein <amirhoseinbu80@gmail.com>
2024-05-21 19:46:24 +0330

    build a blog_list view

--- weblog/views.py
+++ weblog/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.decorators.http import require_http_methods
+from .models import Blog,Category,Tag,Comment
 # Create your views here.
 
-require_http_methods(["GET"])
+@require_http_methods(["GET"])
 def index(request):
 
     context={}
 
     return render(request,'blog/home.html',context)
+
+
+@require_http_methods(['GET'])
+def blogs_list(request,category_slug= None):
+
+    category = None
+    categories = Category.objects.all()
+    blog = Blog.objects.all()
+
+    if category_slug:
+
+        category = get_object_or_404(Category,slug = category_slug)
+        blog = Blog.objects.filter(category = category)
+
+    context = {
+        'categories':categories,
+        'blog':blog,
+        'category': category,
+
+    }
+
+    return render (request,'blog/list.html',context)
+
