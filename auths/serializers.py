d33b031ee38a0a65e85f3c047a8ef283ca804f83
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-28 02:09:29 +0330

    build UserSerializer

---
+++ auths/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from . models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = CustomUser
+        fields = [
+
+            'first_name','last_name',
+            'username','password','email'
+        ]
