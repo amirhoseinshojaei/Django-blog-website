b7607d4b73c36db3efd07b65c0de5fd7f5647284
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-28 11:34:36 +0330

    update UserSerializer

--- auths/serializers.py
+++ auths/serializers.py
@@ -4,6 +4,11 @@ from . models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
 
+    first_name = serializers.CharField(max_lenght = 255 , read_only = True)
+    last_name = serializers.CharField(max_length = 255 , read_only = True)
+    username = serializers.CharField(max_length=255, read_only=True)
+    email = serializers.EmailField(max_length = 255, read_only = True)
+
     class Meta:
 
         model = CustomUser
