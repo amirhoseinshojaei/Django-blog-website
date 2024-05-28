0b2fd2f4f6f6712cfc378dad426db85212a144df
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-28 15:27:06 +0330

    configure restframework url

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/',include('rest_framework.urls',namespace='rest_framework')),
     path('auth/',include('auths.urls',namespace='auth')),
     path('',include('weblog.urls',namespace='blog')),
 ]
